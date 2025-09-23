a686acc4c1355e9a0058e54b8fb77d27c3fdbac7
Anton Oresten <antonoresten@gmail.com>
2025-09-23 22:19:15 +0200

    Update modular branch (#27)
    * Add zero_centered RMSNorm option (#26)
    
    * organize tests

--- src/norm/RMSNorm.jl
+++ src/norm/RMSNorm.jl
@@ -1,15 +1,20 @@
 """
-    RMSNorm(dim::Int; eps::T=1f-5)
+    RMSNorm(dim::Int; T=Float32, eps=1f-5, zero_centered=false)
 
 Root Mean Square Layer Normalization. As used in Llama3.
 """
 @concrete struct RMSNorm
     weight
     eps
+    offset
 end
 
 Flux.@layer RMSNorm
 
-RMSNorm(dim::Int; eps::T=1f-5) where T = RMSNorm(ones(T, dim), eps)
+function RMSNorm(dim::Int; T=Float32, eps=1f-5, zero_centered=false)
+    weight = zero_centered ? zeros(T, dim) : ones(T, dim)
+    offset = zero_centered ? one(T) : zero(T)
+    RMSNorm(weight, T(eps), offset)
+end
 
-(norm::RMSNorm)(x) = Ops.rms_norm(x, norm.weight; norm.eps)
+(norm::RMSNorm)(x) = Ops.rms_norm(x, norm.weight; norm.eps, norm.offset)
---
+++ test/shared.jl
@@ -0,0 +1,26 @@
+@testset "shared.jl" begin
+
+    @testset "like" begin
+        v = rand(Int, 16)
+        @test all(x -> x === 1.0f0, like(1.0f0, v, Float32, 2, 1))
+        @test all(x -> x === 1.0f0, like(1, v, Float32, 4))
+        @test all(x -> x === 1, like(1f0, v, 4))
+        @test all(x -> x === 1, like(1f0, v))
+
+        @test all(x -> x === 0f0, zeros_like(v, Float32, 20))
+        @test all(x -> x === 1f0, ones_like(v, Float32, 20))
+        @test all(x -> x === false, falses_like(v, 20))
+        @test all(x -> x === true, trues_like(v, 20))
+
+        @test let r = like(1:5, v)
+            r == 1:5 && r isa Vector{Int}
+        end
+        @test let r = like(1:5, v, Float32)
+            r == 1:5 && r isa Vector{Float32}
+        end
+        @test let r = like(1.0:5.0, v)
+            r == 1:5 && r isa Vector{Float64}
+        end
+    end
+
+end
