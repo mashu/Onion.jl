267fe370f8d06d76792c5a24bc8a4cada5e14554
Anton Oresten <antonoresten@proton.me>
2025-09-24 17:20:44 +0200

    update

--- test/norm/norm.jl
+++ test/norm/norm.jl
@@ -1,7 +1,7 @@
 @testset "norm.jl" begin
 
     @testset "LpNorm" begin
-        v = randn(Float32, 100, 200)
+        v = randn(Float32, 1000, 200)
         atol = 1f-2
         @testset for p in 1:5, dims in [(1,2), 1, ()]
             s = sum(x -> abs(x)^p, LpNorm(p; dims)(v); dims)
