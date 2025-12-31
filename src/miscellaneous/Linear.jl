ead56d142f40e6d4da789f04ded961a024fdb101
AntonOresten <antonoresten@proton.me>
2025-12-31 19:23:44 +0100

    Use Rewrap v0.1.7

--- src/miscellaneous/Linear.jl
+++ src/miscellaneous/Linear.jl
@@ -27,7 +27,7 @@ end
 function ((; weight, bias, σ)::Linear)(x)
     x′ = reshape(x, Keep(), :)
     y′ = weight * x′
-    y = reshape(y′, Keep(), Split(1, size(x)[2:end]))
+    y = reshape(y′, Keep(), size(x)[2:end]...)
     NNlib.bias_act!(σ, y, bias)
     return y
 end
