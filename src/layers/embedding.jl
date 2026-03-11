a215721b9bf5df5fb66ff73127e44821def8f8e2
AntonOresten <antonoresten@proton.me>
2026-03-11 14:39:09 +0100

    use Rewrap for Embedding

--- src/layers/embedding.jl
+++ src/layers/embedding.jl
@@ -6,11 +6,11 @@ Embedding((in, out)::Pair{Int,Int}; init = →(Float32) ∘ randn) = Embedding(i
 
 (m::Embedding)(x::Integer) = m.weight[:, x]
 (m::Embedding)(x::AbstractVector) = NNlib.gather(m.weight, x)
-(m::Embedding)(x::AbstractArray) = reshape(m(vec(x)), :, size(x)...)
+(m::Embedding)(x::AbstractArray) = reshape(m(Rewrap.vec(x)), Keep(), size(x)...)
 
 (m::Embedding)(x::AbstractVector{Bool}) = m.weight * x  # usually OneHotVector
 (m::Embedding)(x::AbstractMatrix{Bool}) = m.weight * x  # usually OneHotMatrix
-(m::Embedding)(x::AbstractArray{Bool}) = reshape(m(reshape(x, size(x,1), :)), :, size(x)[2:end]...)
+(m::Embedding)(x::AbstractArray{Bool}) = reshape(m(reshape(x, Keep(), :)), Keep(), size(x)[2:end]...)
 
 function Base.show(io::IO, m::Embedding)
     print(io, "Embedding(", size(m.weight, 2), " => ", size(m.weight, 1), ")")
